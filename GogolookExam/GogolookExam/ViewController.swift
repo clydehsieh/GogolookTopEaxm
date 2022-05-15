0a2bc0f643ff624450d2071fc769e04077ad3bba
clydehsieh <clydehsieh0601@gmail.com>
2022-05-15 12:37:40 +0800

    init

---
+++ GogolookExam/GogolookExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GogolookExam
+//
+//  Created by ClydeHsieh on 2022/5/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GogolookExam/GogolookExamUITests/GogolookExamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GogolookExamUITestsLaunchTests.swift
+//  GogolookExamUITests
+//
+//  Created by ClydeHsieh on 2022/5/15.
+//
+
+import XCTest
+
+class GogolookExamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
